25e5ba688914657f4f5ee7184e8fcf7a5144fd3a
name <rk6318633@gmail.com>
2024-11-13 10:35:35 +0530

    added db layer and basic routing layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { StatusCodes } from "http-status-codes";
 
+import connectDB from "./config/dbConfig.js";
 import { PORT } from "./config/serverConfig.js";
+import apiRouter from './routes/apiRoutes.js';
 
 
 
 const app = express();
+app.use(express.urlencoded({extended:true}))
 
+app.use('/api',apiRouter);
 
 app.get("/ping", (req, res) => {
   return res.status(StatusCodes.OK).json({ message: "pong" });
@@ -14,4 +18,5 @@ app.get("/ping", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
---
+++ backend/src/routes/v1/users.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+
+return res.status(StatusCodes.OK).json({message:'get/users'});
+})
+
+export default router;
