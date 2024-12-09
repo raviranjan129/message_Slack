236d5f077438550214f0b5eb7d3e4c3e90bc855a
name <rk6318633@gmail.com>
2024-12-09 12:22:53 +0530

    added socket.io setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 
 import express from "express";
+import { createServer } from 'http';
 import { StatusCodes } from "http-status-codes";
+import {Server} from 'socket.io';
 
 import bullServerAdapter from "./config/bullBoardConfig.js";
 import connectDB from "./config/dbConfig.js";
@@ -10,7 +12,8 @@ import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
 
-
+const server = createServer(app);
+const io=new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -25,7 +28,17 @@ app.get("/ping", (req, res) => {
   return res.status(StatusCodes.OK).json({ message: "pong" });
 });
 
-app.listen(PORT, async() => {
+io.on('connection',(socket)=>{
+  console.log('a user connected',socket.id);
+  
+  socket.on('messageFromClient',(data)=>{
+    console.log('Message from client ',data);
+  
+    io.emit('new message',data.toUpperCase());
+  })
+})
+
+server.listen(PORT, async() => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 
